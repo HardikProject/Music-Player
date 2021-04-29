29e06d3e9cdae5a845dfe63e7397ae566d32ccad
Hardik <19it.hardikprajapati@adit.ac.in>
2021-04-29 16:37:13 +0530

    UI improve

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { FcMusic } from "react-icons/fc";
+import { FcMusic , FcHome } from "react-icons/fc";
 import '../styles/Nav.scss'
 
 function Nav({libraryActive,setLibraryActive}) {
     return (
         <div className='Nav'>
             <h1>Chill Music Player</h1>
-            <button onClick={()=>setLibraryActive(!libraryActive)} > <FcMusic id='Nav__icon' size='1.5em'/>Library</button>
+            <button onClick={()=>setLibraryActive(!libraryActive)} >{libraryActive ? <FcHome id='Nav__icon' size='1.5em'/>:<FcMusic id='Nav__icon' size='1.5em'/>} </button>
         </div>
     )
 }
